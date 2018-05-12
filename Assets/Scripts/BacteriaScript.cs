ac43cf5107484554b3d63c67c13a99551b13bc52
Halbschatten <marco-solano@hotmail.com>
2018-05-12 20:02:20 -0300

    Base Project W/ new player Items (monocle update)

---
+++ Assets/Scripts/BacteriaScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BacteriaScript : MonoBehaviour {
+
+	private GameObject gameControllerGameObject; //Reference to the Game Controller GameObject;
+	private string gameControllerTag = "GameController"; //Game Controller's tag;
+	private float life = 5.0f;
+	private int scoreOnDeath = 125;
+	public string bulletTag = "Bullet";
+	private PhysicsScript physs;
+	public string[] playerTags;
+	public GameObject player;
+	public bool followPlayer = true;
+	public float followSpeedMultiplier = 0.009f;
+	public float followSpeed = 5.0f;
+    private static float defaultDamage = 1.0f;
+    public bool debuffTakeMoreDamage = true;
+    public float debuffTakeMoreDamageMultiplier = 1.75f;
+
+	GameObject FindOneRandomPlayer()
+	{
+		PlayerScript[] players = GameObject.FindObjectsOfType<PlayerScript> ();
+		try
+		{
+			int pos = Random.Range (0, players.Length);
+			return players[pos].gameObject;
+		}
+		catch 
+		{
+			this.enabled = false; 
+			return null;
+		}
+	}
+
+	void Awake()
+	{	
+		gameControllerGameObject = GameObject.FindGameObjectWithTag (gameControllerTag);
+		physs = this.gameObject.GetComponent<PhysicsScript> ();	
+		player = FindOneRandomPlayer ();
+	}
+	// Use this for initialization
+	void Start ()
+	{
+		
+
+	}
+	void FixedUpdate()
+	{
+		if (followPlayer == true)
+		{
+			if (player != null) 
+			{
+				transform.rotation = physs.LookAt2D (this.gameObject, player);
+				transform.Translate (Vector2.right * followSpeed * followSpeedMultiplier);
+			}
+			else 
+			{
+				player = FindOneRandomPlayer ();
+			}
+		}
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == bulletTag)
+		{
+            if (debuffTakeMoreDamage == true)
+            {
+                this.life -= defaultDamage * debuffTakeMoreDamageMultiplier;
+            }
+            else
+            {
+                this.life -= defaultDamage;
+            }
+			Destroy (other.gameObject);
+			if (this.life <= 0.0f) 
+			{
+				Destroy (this.gameObject);
+				gameControllerGameObject.GetComponent<GameControllerScript> ().Score += scoreOnDeath;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public float bgScrollSpeed;
+    public GameObject mainMenu, playMenu, tutorialMenu, settingsMenu, aboutMenu;
+
+    void Update()
+    {
+        //If ESC key is pressed, quit application.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+	public void MainMenuButtonPlay()
+	{
+        mainMenu.SetActive(false);
+        playMenu.SetActive(true);
+		//SceneManager.LoadScene("DemoScene");
+	}
+	public void MainMenuButtonTutorial()
+	{
+        mainMenu.SetActive(false);
+        tutorialMenu.SetActive(true);
+    }
+	public void MainMenuButtonSettings()
+	{
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+    public void MainMenuButtonAbout()
+    {
+        mainMenu.SetActive(false);
+        aboutMenu.SetActive(true);
+    }
+    public void MainMenuButtonExit()
+	{
+		QuitGame();
+	}
+    public void PlayMenuBackButton()
+    {
+        playMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void TutorialMenuBackButton()
+    {
+        tutorialMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void SettingsMenuBackButton()
+    {
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void AboutMenuBackButton()
+    {
+        aboutMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void QuitGame()
+	{
+     // save any game data here
+     #if UNITY_EDITOR
+         // Application.Quit() does not work in the editor so
+         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+         UnityEditor.EditorApplication.isPlaying = false;
+     #else
+         Application.Quit();
+     #endif
+	}
+}
