d384d399d09837c9bb43d0c57509be5c3455a732
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2018-05-20 18:14:47 -0300

    buffAutoshoot Powerup implemented

---
+++ Assets/Scripts/PlayerBuffAutoshoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBuffAutoshoot : MonoBehaviour
+{
+    private GameObject gameControllerGameObject; //Reference to the Game Controller GameObject;
+    private GameControllerScript gameControllerScript;
+    private string gameControllerTag = "GameController"; //Game Controller's tag;
+    private string playerTag = "Player";
+    public float multiplier = 1.5f;
+    public float duration = 30.0f;
+
+    void Awake()
+    {
+        gameControllerGameObject = GameObject.FindGameObjectWithTag(gameControllerTag);
+        gameControllerScript = gameControllerGameObject.GetComponent<GameControllerScript>();
+    }
+
+    // Use this for initialization
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == playerTag)
+        {
+            for (int i = 0; i < gameControllerScript.Players.Length; i++)
+            {
+                gameControllerScript.Players[i].GetComponent<PlayerScript>().BuffAutoShoot(duration);
+                //Debug.Log(string.Format("[Player {0}]: [{1}, {2:00}s, {3:#.##}x] activated!", i + 1, "buffAutoShoot", duration, multiplier));
+            }
+            GetComponent<CircleCollider2D>().enabled = false;
+            StartCoroutine(WaitNSecondsAndDestroy(0.5f));
+        }
+    }
+
+    IEnumerator WaitNSecondsAndDestroy(float n)
+    {
+        yield return new WaitForSeconds(n);
+        Destroy(this.gameObject);
+    }
+}
