0a2ddd5e2e1d142342bcbd3d6619540c58401b22
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2018-05-23 22:38:24 -0300

    Highscore Menu now orders correctly

--- Assets/Scripts/Highscore.cs
+++ Assets/Scripts/Highscore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 public class Highscore
 {
+    
     string player1Name, player2Name;
     int score;
 
@@ -12,22 +13,26 @@ public class Highscore
         player2Name = "";
         score = 0;
     }
-    public string GetPlayer1Name()
-    {
-        return player1Name;
-    }
-    public string GetPlayer2Name()
-    {
-        return player2Name;
-    }
-    public int GetScore()
-    {
-        return score;
-    }
+
     public Highscore(string player1Name, string player2Name, int score)
     {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
         this.score = score;
+    }
+
+    public string GetPlayer1Name()
+    {
+        return player1Name;
+    }
+
+    public string GetPlayer2Name()
+    {
+        return player2Name;
+    }
+
+    public int GetScore()
+    {
+        return score;
     }
 }
--- Assets/Scripts/HighscorePrompt.cs
+++ Assets/Scripts/HighscorePrompt.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class HighscorePrompt : MonoBehaviour
 {
+    void HandleFunc(Highscore arg1)
+    {
+    }
+
+
     public TMP_Text[] player1Name;
     public TMP_Text[] player2Name;
     public TMP_Text score;
@@ -15,6 +22,7 @@ public class HighscorePrompt : MonoBehaviour
 
     void Awake ()
     {
+        highscoresList = ReadHighscoresFromPlayerPrefs();
         gameControllerScript = FindObjectOfType<GameControllerScript>();
         score.text += gameControllerScript.Score;
         highscoreListText.text = List10Highscores();
@@ -35,14 +43,28 @@ public class HighscorePrompt : MonoBehaviour
 
     public void SaveHighscoreInPlayerPrefs(List<Highscore> input)
     {
-        //input.Sort();
+        input = OrderByDescend(input);
         for(int i = 0; i < input.Count; i++)
         {
             PlayerPrefs.SetString("Highscore_" + i + "_Player1Name", input[i].GetPlayer1Name());
             PlayerPrefs.SetString("Highscore_" + i + "_Player2Name", input[i].GetPlayer2Name());
             PlayerPrefs.SetInt("Highscore_" + i + "_Score", input[i].GetScore());
+            PlayerPrefs.SetInt("Highscore_Count", input.Count);
         }
+    }
 
+    public List<Highscore> ReadHighscoresFromPlayerPrefs()
+    {
+        int highscoreCount =  PlayerPrefs.GetInt("Highscore_Count");
+        List<Highscore> aux = new List<Highscore>();
+        for (int i = 0; i < highscoreCount; i++)
+        {
+            aux.Add(new Highscore(PlayerPrefs.GetString("Highscore_" + i + "_Player1Name"), PlayerPrefs.GetString("Highscore_" + i + "_Player2Name"), PlayerPrefs.GetInt("Highscore_" + i + "_Score")));
+        }
+        foreach (Highscore h in aux){
+            print(string.Format("{0} | {1}  | {2}", h.GetPlayer1Name(), h.GetPlayer2Name(), h.GetScore()));
+        }
+        return aux;
     }
 
     public string List10Highscores()
@@ -55,5 +77,10 @@ public class HighscorePrompt : MonoBehaviour
             output = output + string.Format("{0:D9}\n", PlayerPrefs.GetInt("Highscore_" + i + "_Score"));
         }
         return output;
+    }
+
+    public List<Highscore> OrderByDescend (List<Highscore> input)
+    {
+        return input.OrderByDescending(h => h.GetScore()).ToList();
     }
 }
