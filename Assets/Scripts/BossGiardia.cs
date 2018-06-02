b8938cb887d92a1ca044f7b46912962db5e26af8
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2018-06-02 18:44:50 -0300

    Implemented the 2nd boss sprite

--- Assets/Scripts/BossGiardia.cs
+++ Assets/Scripts/BossGiardia.cs
@@ -11,7 +11,6 @@ public class BossGiardia : MonoBehaviour
     private float life = defaultLife;
     private int scoreOnDeath = 120000;
     public string bulletTag = "Bullet";
-    public Animator animator;
     public GameObject[] gameObjectsToEnableOnDestroy;
 
     private void Awake()
@@ -64,7 +63,6 @@ public class BossGiardia : MonoBehaviour
         if (other.gameObject.tag == bulletTag && GetComponent<MoveToTheScene>().movementEnabled == false)
         {
             this.life -= other.gameObject.GetComponent<BulletScript>().damage * 0.75f;
-            animator.Play("Boss_Face_Angry");
             if (this.life <= 0.0f)
             {
                 gameControllerScript.BossHealth = life;
---
+++ Assets/Scripts/EyesLerpRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyesLerpRotation : MonoBehaviour 
+{
+    float rotation;
+    float time = 1.0f;
+    public float minAngle = -10.0f;
+    public float maxAngle = 90.0f;
+    public bool flip = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (flip == true)
+        {
+            rotation = Mathf.LerpAngle(minAngle, maxAngle, Mathf.PingPong(Time.time, time));
+        }
+        else
+        {
+            rotation = -Mathf.LerpAngle(minAngle, maxAngle, Mathf.PingPong(Time.time, time));
+        }
+        this.transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, rotation + transform.parent.transform.eulerAngles.z); 
+	}
+}
